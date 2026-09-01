303dec53bc43e07e5d308cf498a62e8da74d69d7
ankurbag6 <bagankur@gmail.com>
2026-09-01 13:20:31 -0700

    feat: add new interview mocks

---
+++ 31-dialpad-practise/designhitcounter.js
@@ -0,0 +1,68 @@
+/*
+362. Design Hit Counter
+Problem Description
+You need to design a hit counter system that tracks the number of hits received within the past 5 minutes (300 seconds).
+
+The system should support two main operations:
+
+Recording hits: When a hit occurs at a specific timestamp (in seconds), the system should record it. Multiple hits can happen at the same timestamp.
+
+Querying hit count: Given a timestamp, the system should return the total number of hits that occurred in the past 300 seconds from that timestamp. Specifically, it counts all hits in the time range [timestamp - 299, timestamp].
+
+Key constraints and assumptions:
+
+Timestamps are provided in seconds
+Calls to the system happen in chronological order (timestamps are monotonically increasing)
+Multiple hits may arrive at the same timestamp
+The HitCounter class needs three methods:
+
+HitCounter(): Initializes the hit counter system
+hit(timestamp): Records a hit at the given timestamp
+getHits(timestamp): Returns the count of all hits in the past 300 seconds from the given timestamp
+For example, if hits occurred at timestamps 1, 2, 3, and 301, calling getHits(301) would return 1 (only the hit at timestamp 301 is within the past 300 seconds), while getHits(303) would still return 1 since the hit at timestamp 1 is now more than 300 seconds old.
+*/
+
+class HitCounter {
+    constructor() {
+        this.store = Map();
+    }
+
+    hit(timestamp) {
+        const records = this.store.get(timestamp);
+        this.store.set(timestamp, (records === undefined) ? 1 : records+1 );
+    }
+
+    getHits(timestamp) {
+        // if timestamp >300
+        // for()
+        for (let i = 0; i < 300; i++) {
+            if (timestamp - this.store.get(i) < 300) total += this.counts[i];
+        }
+        return total;
+    }
+}
+
+
+// Solution
+class HitCounter {
+    constructor() {
+        this.times  = new Array(300).fill(0);   // which second each bucket holds
+        this.counts = new Array(300).fill(0);   // hits in that second
+    }
+    hit(timestamp) {
+        const i = timestamp % 300;
+        if (this.times[i] !== timestamp) {      // bucket is stale -> overwrite
+            this.times[i] = timestamp;
+            this.counts[i] = 1;
+        } else {
+            this.counts[i]++;                   // same second -> accumulate
+        }
+    }
+    getHits(timestamp) {
+        let total = 0;
+        for (let i = 0; i < 300; i++) {
+            if (timestamp - this.times[i] < 300) total += this.counts[i];
+        }
+        return total;
+    }
+}
---
+++ 31-dialpad-practise/minstack.js
@@ -0,0 +1,103 @@
+/*
+155. Min Stack
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int value) pushes the element value onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+Example 1: 
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+ 
+
+Constraints:
+
+-231 <= val <= 231 - 1
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+At most 3 * 104 calls will be made to push, pop, top, and getMin.
+*/
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.mins = [];
+    }
+    push(value) {
+        this.stack.push(value);
+        this.mins.push( this.mins.length === 0 ? value : Math.min(this.getMin(), value));
+    }
+    pop() {
+        this.mins.pop();
+        return this.stack.pop();
+    }
+    top() {
+        return this.stack[this.stack.length-1];
+    }
+    getMin() {
+        return this.mins[this.mins.length - 1];
+    }
+}
+const minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// console.log(minStack.stack);
+// console.log(minStack.mins);
+// console.log(minStack.getMin()); // return -3
+// minStack.pop();
+// console.log(minStack.top());    // return 0
+// console.log(minStack.getMin()); // return -2
+
+minStack.push(3);
+minStack.push(2);
+minStack.push(1);
+minStack.push(5);
+minStack.push(-4);
+minStack.push(2);
+console.log(minStack.stack);
+console.log(minStack.mins);
+console.log(minStack.getMin()); // return 1
+minStack.pop();
+console.log(minStack.top());    // return 0
+minStack.pop();
+console.log(minStack.getMin()); // return -2
+
+// O(1) space 
+
+var MinStack = function() { this.s = []; this.minEle = null; };
+MinStack.prototype.push = function(val) {
+    if (!this.s.length) { this.s.push(val); this.minEle = val; }
+    else if (val < this.minEle) { this.s.push(2*val - this.minEle); this.minEle = val; }
+    else this.s.push(val);
+};
+MinStack.prototype.pop = function() {
+    let t = this.s.pop();
+    if (t < this.minEle) this.minEle = 2*this.minEle - t;
+};
+MinStack.prototype.top = function() {
+    let t = this.s[this.s.length-1];
+    return t < this.minEle ? this.minEle : t;
+};
+MinStack.prototype.getMin = function() { return this.minEle; };
+
+
